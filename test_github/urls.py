33a1de2428a050abada6ff07953d80d90b5ecade
DyedykStanislav <dyedyk.stanislav@datawiz.io>
2021-04-26 17:39:53 +0300

    init commit

--- test_github/urls.py
+++ test_github/urls.py
@@ -16,11 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-
-fdg
-
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-
+asdf = "sasdfasdfjsdlfkjhskdfasdfjsdlfkjhskdfasdfjsdlfkjhskdfasdfjsdlfkjhskdfasdfjsdlfkjhskdfasdfjsdlfkjhskdfasdfjsdlfkjhskdfasdfjsdlfkjhskj"
