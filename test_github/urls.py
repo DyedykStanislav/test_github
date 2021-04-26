408e41bd645137d630d45e72458f1582c8a879b6
DyedykStanislav <dyedyk.stanislav@datawiz.io>
2021-04-26 18:25:25 +0300

    init commit

--- test_github/urls.py
+++ test_github/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 
 asdf = 'asdflkajsdhflkjashdflkjadshnflkjanbdkbfgnfkgbadsflkgjbdflkgjfgnfkgbadsflkgjbdflkgjfgnfkgbadsflkgjbdflkgjfgnfkgbadsflkgjbdflkgjn'
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
