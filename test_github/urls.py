62f95c7f384799dc7775ab739b148f64f517d0b8
DyedykStanislav <dyedyk.stanislav@datawiz.io>
2021-04-26 09:18:39 +0300

    init commit

--- test_github/urls.py
+++ test_github/urls.py
@@ -26,7 +26,7 @@ fufguyj = adsfadfa =asdfasdfa=sdf=dasd=fasdf=asfdasdfa =asasdf
 fufguyj = adsfadfa =asdfasdfa\
     =sdf=dasd=fasdf=asfdasdfa =asasdf
 uyj = adsfadfa =asdfasdfa=sdf=dasd=fasdf=asfdasdfa =asuyj = adsfadfa =asdfasdfa=sdf=dasd=fasdf=asfdasdfa =asuyj = adsfadfa =asdfasdfa=sdf=dasd=fasdf=asfdasdfa =asuyj = adsfadfa =asdfasdfa=sdf=dasd=fasdf=asfdasdfa =as
-
+adf
 
 
 urlpatterns = [
