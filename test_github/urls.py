8db28f80866bd6bff442167c595df80e4935a800
DyedykStanislav <dyedyk.stanislav@datawiz.io>
2021-04-27 10:13:09 +0300

    init commit

--- test_github/urls.py
+++ test_github/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-
+super = "pasdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
 
 
 
